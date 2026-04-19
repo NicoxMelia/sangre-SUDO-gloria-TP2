c7cafccc179131246de613015b8dbb37de82f21f
Baltasar <bmottura@mi.unc.edu.ar>
2026-04-18 23:28:34 -0300

    Implementacion de subrrutina en ASM; modificacion de receiver.c para que llame a la subrutina ASM

--- src/c/receiver.c
+++ src/c/receiver.c
@@ -1,14 +1,14 @@
 # include <stdio.h>
 
 // Descomentar cuando esté lista la implementación en ASM.
-// extern int process_value(float value);
+extern int process_value(float value1,float value2,float value3,float value4,float value5,float value6,float value7,float value8, double value9);
 
-int mock_process_value(double value) {
-    return (int) value + 1;
-}
+//int mock_process_value(double value) {
+ //   return (int) value + 1;
+//}
 
 int get_value_processed(double value) {
-    return mock_process_value(value);
+    //return mock_process_value(value);
     // Cuando ASM esté listo, cambiar por:
-    // return process_value(value);
+    return process_value(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, value);
 }
